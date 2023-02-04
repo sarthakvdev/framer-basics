86eaa97494f843e84db5aabc46151441447a94af
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2023-02-04 23:55:05 +0530

    saturday

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Framer Motion 101</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -55,16 +55,9 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Framer!
           </a>
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
 
         <motion.ul
           animate="hidden"
